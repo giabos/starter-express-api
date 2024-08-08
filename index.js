c1ddf624ce403f6d70df2b45823fc301ead661f2
Gio <gianni.boscarino@gmail.com>
2024-08-08 22:15:48 +0200

    catch error & return empty response

--- index.js
+++ index.js
@@ -11,12 +11,9 @@ const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('db.sqlite3');
 
-
 app.use(cors());
 //app.use(bodyParser.text());
-app.use(express.text())
-
-
+app.use(express.text());
 
 async function scrapeProfile(url) {
     //console.log("--", url);
@@ -25,12 +22,10 @@ async function scrapeProfile(url) {
 
     const text = $('.phone-bar').text();
     const dateStr = /\s+(à|om|op|le)\s(.*)$/.exec(text)[2];
-
     const profileImgUrl = $('.avatar-wrapper img').attr('src');
-
     const location = $('.article-subtitle a').first().text();
 
-    return ({ dateStr, profileImgUrl, location, url });
+    return { dateStr, profileImgUrl, location, url };
 }
 
 async function scrapeList(url) {
@@ -49,41 +44,51 @@ app.get('/status', (req, res) => {
 
 app.get('/scrape', async (req, res) => {
     const list = await scrapeList(req.query.url);
-    const resp = await Promise.all(list.map(url => scrapeProfile(url)));
+    const resp = await Promise.all(list.map((url) => scrapeProfile(url)));
     res.contentType = 'application/json';
-    res.send(resp.filter(a => !!a.location));
+    res.send(resp.filter((a) => !!a.location));
 });
 
 app.get('/scrape-one', async (req, res) => {
-    const data = await scrapeProfile(req.query.url);
-    res.json(data);
+    try {
+        const data = await scrapeProfile(req.query.url);
+        res.json(data);
+    } catch (e) {
+        res.json({
+            dateStr: '-',
+            profileImgUrl:
+                'https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg',
+            location: '-',
+            url: req.query.url,
+        });
+    }
 });
-
-
 
 /// key-value storage api.
 
 app.get('/store/:id', function (req, res) {
-    db.get("SELECT code FROM store where rowid=" + req.params.id, (err, row) => {
-        res.send(row.code);
-    });
+    db.get(
+        'SELECT code FROM store where rowid=' + req.params.id,
+        (err, row) => {
+            res.send(row.code);
+        }
+    );
 });
 
 app.post('/store', async function (req, res) {
-    db.run("CREATE TABLE if not exists store (code TEXT)");
-    const stmt = db.prepare("INSERT INTO store VALUES (?) returning rowid");
+    db.run('CREATE TABLE if not exists store (code TEXT)');
+    const stmt = db.prepare('INSERT INTO store VALUES (?) returning rowid');
     stmt.run(req.body, function () {
-        res.json({ success: true, id: this.lastID })
+        res.json({ success: true, id: this.lastID });
     });
     stmt.finalize();
 });
 
 app.put('/store/:id', async function (req, res) {
-    const stmt = db.prepare("update store set code = ? where rowid = ?");
+    const stmt = db.prepare('update store set code = ? where rowid = ?');
     stmt.run(req.body, req.params.id);
     stmt.finalize();
-    res.json({ success: true })
+    res.json({ success: true });
 });
 
-
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
